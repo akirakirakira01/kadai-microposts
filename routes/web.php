ce2bb6c7bdc3259882572f484df750348e56762f
あきら <akirakirakira0139@gmail.com>
2023-03-20 15:19:25 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,9 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController; // 追記
 use App\Http\Controllers\MicropostsController; //追記
+use App\Http\Controllers\UserFollowController;  // 追記
 
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', [MicropostsController::class, 'index']);
 
@@ -30,8 +20,19 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+
+
 Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {                                          // 追記
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');         // 追記
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow'); // 追記
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings'); // 追記
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');    // 追記
+    });      
+    
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });                  
 
+
+ 
